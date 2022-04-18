80fbd17af51c3300a34b4e44af9ab93509f34d35
huichao6 <huichao6@staff.weibo.com>
2022-04-18 12:14:21 +0800

    feat: filterBar半成品

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="menu-item">
-    <span class="menu-item__text">{{ text }}</span
-    ><span v-if="showArrow" class="menu-item__arrow"></span>
+  <div ref="menu-item" class="menu-item" @click="handleMenuItem">
+    <span
+      v-if="showArrow"
+      :class="['menu-item__arrow', arrowUp && 'menu-item__arrow-up']"
+    ></span>
+    <span class="menu-item__text">{{ text }}</span>
   </div>
 </template>
 <script>
@@ -16,8 +19,62 @@ export default {
       type: Boolean,
       default: false,
     },
+    defaultArrowUp: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      arrowUp: false,
+    };
+  },
+  watch: {
+    defaultArrowUp: {
+      handler(newVal) {
+        this.arrowUp = newVal;
+      },
+      immediate: true,
+    },
+  },
+  methods: {
+    handleMenuItem() {
+      this.arrowUp = !this.arrowUp;
+      this.$refs['menu-item'].scrollIntoView();
+      this.$emit('handleMenuItem', this.arrowUp);
+    },
   },
 };
 </script>
 <style lang="scss" scoped>
+.menu-item {
+  display: inline-block;
+  font-size: 0;
+  overflow: hidden;
+
+  span {
+    display: inline-block;
+    font: 24px/28px a;
+  }
+
+  &__text {
+    max-width: 80px;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+
+  &__arrow {
+    float: right;
+    margin: 12px 0 0 4px;
+    border-width: 6px;
+    border-style: solid;
+    border-color: #06a43a transparent transparent transparent;
+  }
+
+  &__arrow-up {
+    margin: 6px 0 0 4px; // 12 - border-width;
+    border-color: transparent transparent yellowgreen transparent;
+  }
+}
 </style>
--- src/views/DropMenu/DropMenu.vue
+++ src/views/DropMenu/DropMenu.vue
@@ -1,11 +1,41 @@
 <template>
   <div class="drop-menu">
-    <div class="drop-menu__menu" v-for="(tab, index) in tabs" :key="index">
-      <div class="drop-menu__item">
-        <menu-item :text="tab.name" :showArrow="true"></menu-item>
+    <div ref="menuWrap" class="drop-menu__wrap">
+      <div class="drop-menu__menu">
+        <div class="drop-menu__item" v-for="(tab, index) in tabs" :key="index">
+          <menu-item
+            :text="tab.name"
+            :showArrow="true"
+            :defaultArrowUp="currTab === tab.name"
+            @handleMenuItem="handleMenuItem(tab, $event)"
+          ></menu-item>
+        </div>
       </div>
     </div>
-    <div class="drop-menu__list"></div>
+    <div class="drop-menu__menu-content">
+      <div
+        class="drop-menu__mask"
+        v-for="(tab, index) in tabs"
+        v-show="currTab === tab.name && showMenu"
+        :ref="`mask${tab.name}`"
+        :key="index"
+        :style="{
+          top: `${top}px`,
+        }"
+      >
+        <div class="drop-menu__over"></div>
+        <div class="drop-menu__list">
+          <div
+            class="drop-menu__list-item"
+            v-for="(item, index) in tab.option"
+            :key="index"
+            @click="handleOptionClick(item, tab)"
+          >
+            {{ item.text }}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -18,7 +48,11 @@ export default {
     return {
       tabs: [
         {
-          name: 'tab1',
+          name: 'tabtabtabtabtab1',
+          option: [
+            { text: 'option1', key: '1' },
+            { text: 'option2', key: '2' },
+          ],
         },
         {
           name: 'tab2',
@@ -26,8 +60,37 @@ export default {
         {
           name: 'tab3',
         },
+        {
+          name: 'tab4',
+        },
       ],
+      top: 0,
+      currTab: '',
+      showMenu: false,
     };
+  },
+  mounted() {
+    const menuElem = this.$refs.menuWrap;
+    const { height, top } = (menuElem && menuElem.getBoundingClientRect()) || { height: 0, top: 0 };
+    console.log('test menuElem', menuElem, height, top);
+
+    this.top = height + top || 0;
+  },
+  methods: {
+    handleMenuItem(tab, show) {
+      console.log(show, tab, this.$refs[`mask${tab.name}`]);
+      // const menuElem = this.$refs[`mask${tab.name}`] && this.$refs[`mask${tab.name}`][0];
+      // if (!show) {
+      //   menuElem.style.top = 0;
+      // }
+      this.currTab = tab.name;
+      this.showMenu = show;
+    },
+    handleOptionClick(item, tab) {
+      console.log('select item:', item, tab);
+      this.showMenu = false;
+      this.currTab = '';
+    },
   },
 };
 </script>
