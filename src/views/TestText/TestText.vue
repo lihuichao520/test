f44e6226a0233707ddca108fad4208a7bd68b408
huichao6 <huichao6@staff.weibo.com>
2023-12-28 12:03:15 +0800

    feat: 弹窗实验

--- src/views/TestText/TestText.vue
+++ src/views/TestText/TestText.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="test-text">
-    <div>test text</div>
+  <!-- <div class="test-text"> -->
+    <!-- <div>test text</div>
     <div class="carrying__text">
       美国高洁丝产后卫生巾 4片产妇透气排恶露加长夜安裤安睡
-    </div>
+    </div> -->
 
     <!-- 测试三角形箭头 -->
-    <div class="test-arrow"></div>
+    <!-- <div class="test-arrow"></div>
 
     <div class="test-border-box__wrap">
       <div class="test-border-box"></div>
+    </div> -->
+  <!-- </div> -->
+  <div class="wrap">
+    <div class="content">
+      <div class="detail">jhha好好睡</div>
+
+      <div class="btns">
+        <button>返回直播间</button>
+      </div>
     </div>
   </div>
 </template>
@@ -67,5 +76,79 @@ export default {
   border-color: red #d53128 #d53128 #ff665e;
   border-radius: 6px;
   background: #ff3f35;
+}
+
+// 弹窗
+.wrap {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.4);
+  font-size: 32rpx;
+  z-index: 99;
+
+  .content{
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 30px 0 0;
+    width: 270px;
+    box-sizing: border-box;
+    background-color: #fff;
+    border-radius: 12px;
+    z-index: 99;
+
+    .detail {
+      min-height: 40px;
+      padding: 0 12px;
+      text-align: center;
+      color: #343a40;
+      font-size: 15px;
+      font-weight: bold;
+      box-sizing: border-box;
+    }
+
+    .btns {
+      position: relative;
+      display: flex;
+      justify-content: space-around;
+      margin-top: 20px;
+      // border-top: 1px solid #d0d0d0;
+
+      button {
+        display: inline-block;
+        line-height: 40px;
+        box-sizing: border-box;
+        width: 50%;
+        height: 40px;
+        font-size: 16px;
+        text-align: center;
+        border: none;
+        background: none;
+        border-radius: 0;
+      }
+
+      button:only-child {
+          border-left: none;
+          color: #FF8200;
+      }
+    }
+
+    .btns::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 200%;
+      height: 200%;
+      border-top: 1px solid #d0d0d0;
+      transform-origin: 0 0;
+      transform: scale(0.5, 0.5);
+      box-sizing: border-box;
+    }
+  }
 }
 </style>
